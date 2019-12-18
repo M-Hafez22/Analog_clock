ef26916ef5519b3149c1d16ed72e8526aa9314b5
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2019-12-19 00:13:15 +0200

    Get selectors

---
+++ src/clockApp/config.js
@@ -0,0 +1,8 @@
+// for hands
+export const secondHand = document.getElementById('second');
+export const hourHand = document.getElementById('hour');
+export const minuteHand = document.getElementById('minute');
+// for lines
+export const lines = document.getElementsByClassName("lines");
+export const clock= document.getElementById('clock');
+
