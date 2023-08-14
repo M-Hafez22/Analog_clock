96c95020963808285ab005b2b0b9d9c26302a85d
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2023-08-14 13:43:33 +0300

    handle clock div messing

--- src/clockApp/lines.js
+++ src/clockApp/lines.js
@@ -2,7 +2,13 @@ import {lines, clock} from './config';
 
 export const locatLines = () => {
   for (let i = 1; i < 60; i++) {
+    if(!clock){
+      throw new Error("Clock div not found");
+    }
     clock.appendChild(document.createElement("span"));
+    if(!clock.lastElementChild){
+      throw new Error("Clock div not found");
+    }
     clock.lastElementChild.classList.add('lines');
     lines[i].style.transform = "rotate(" + 6 *  i + "deg)";
   }
